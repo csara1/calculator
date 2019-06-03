4f5c3c9d605ca764472614ec05e71ccd1d8fc1bb
Charalampos (Babis) Sarantoglou <babis.sarantoglou@gmail.com>
2019-06-04 00:54:09 +0300

    Use the stacks and the operator precedence to evaluate the infix expressions entered

--- code.js
+++ code.js
@@ -32,7 +32,7 @@ function display(integer) {
         digits = 1;
     integer /= 10;
     integer = Math.floor(integer);
-    while(integer > 1) {
+    while(integer > 0) {
         if(digits%3 == 0) {
             string  = ',' + string;
         }
@@ -44,60 +44,88 @@ function display(integer) {
     document.getElementById('display').value = string;
 }
 
-function digit(event) {
+function displayFloat(float) {
+    document.getElementById('display').value = float;
+}
+
+function digitClicked(event) {
     operatorSelected = false;
-    displayValue *= 10;
-    displayValue += parseInt(event.srcElement.innerText);
-    display(displayValue);
-    if(displayValue > 1e14) {
+    operand *= 10;
+    operand += parseInt(event.srcElement.innerText);
+    display(operand);
+    if(operand > 1e14) {
         numberButtons.forEach(button => {
-            button.removeEventListener('click', digit);
+            button.removeEventListener('click', digitClicked);
             button.classList.add('disabled');
         });
     }
 }
 
-function operator(event) {
-    if(!operatorSelected) {
-        numberButtons.forEach(button => {
-            button.addEventListener('click', digit);
-            button.classList.remove('disabled');
-        });
-        operatorSelected = true;
-        // use operand
-    } else {
-        // override last operator
+function precedence(operator) {
+    switch (operator) {
+        case '+':
+        case '-':
+            return 0;
+        case '*':
+        case '/':
+            return 1;
     }
-    let currentOperator = event.srcElement.innerText;
-    // use operator
 }
 
-function stack_create () {
+function operatorClicked(event) {
+    if(!operatorSelected) {
+        numberButtons.forEach(button => {
+            button.addEventListener('click', digitClicked);
+            button.classList.remove('disabled');
+        });
+        stackPush(operandStack, operand);
+        operand = 0;
+    } else {
+        stackPop(operatorStack);
+    }
+    let currentOperator = event.srcElement.innerText;
+    if(!stackEmpty(operatorStack) && precedence(currentOperator) <= precedence(stackTop(operatorStack))) {
+        let result;
+        do {
+            let rightOperand = stackPop(operandStack),
+                lastOperator = stackPop(operatorStack),
+                leftOperand = stackPop(operandStack);
+            result = operate(lastOperator, leftOperand, rightOperand);
+            stackPush(operandStack, result);
+        } while (!stackEmpty(operatorStack));
+        displayFloat(result);
+    }
+    stackPush(operatorStack, currentOperator);
+    operatorSelected = true;
+}
+
+function stackCreate () {
     return new Array(0);
 }
 
-function stack_push(stack, element) {
+function stackPush(stack, element) {
     stack.push(element);
 }
 
-function stack_empty(stack) {
+function stackEmpty(stack) {
     return stack.length == 0;
 }
 
-function stack_top(stack) {
+function stackTop(stack) {
     return stack.slice(-1)[0];
 }
 
-function stack_pop(stack) {
+function stackPop(stack) {
     return stack.pop();
 }
 
-let numberButtons = Array.from(document.getElementsByClassName('number')),
-    operatorButtons = Array.from(document.getElementsByClassName('operator')),
-    displayValue = 0,
-    operatorSelected = false
-    operandStack = stack_create(),
-    operatorStack = stack_create();
+const numberButtons = Array.from(document.getElementsByClassName('number')),
+      operatorButtons = Array.from(document.getElementsByClassName('operator')),
+      operandStack = stackCreate(),
+      operatorStack = stackCreate();
+let operand = 0,
+    operatorSelected = false;
+
 display(0);
-numberButtons.forEach(button => button.addEventListener('click', digit));
-operatorButtons.forEach(button => button.addEventListener('click', operator));
+numberButtons.forEach(button => button.addEventListener('click', digitClicked));
+operatorButtons.forEach(button => button.addEventListener('click', operatorClicked));
