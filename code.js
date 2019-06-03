811aaadd92b40ddda1d5091f51f42a9fae708c8e
Charalampos (Babis) Sarantoglou <babis.sarantoglou@gmail.com>
2019-06-03 19:50:10 +0300

    Populate the display when the number buttons are clicked

--- code.js
+++ code.js
@@ -26,3 +26,37 @@ function operate(operator, a, b) {
             return divide(a, b);
     }
 }
+
+function display(integer) {
+    let string = "" + integer%10,
+        digits = 1;
+    integer /= 10;
+    integer = Math.floor(integer);
+    while(integer > 1) {
+        if(digits%3 == 0) {
+            string  = ',' + string;
+        }
+        digits++;
+        string = integer%10 + string;
+        integer /= 10;
+        integer = Math.floor(integer);
+    }
+    document.getElementById('display').value = string;
+}
+
+function digit(event) {
+    displayValue *= 10;
+    displayValue += parseInt(event.srcElement.innerText);
+    display(displayValue);
+    if(displayValue > 1e14) {
+        numberButtons.forEach(button => {
+            button.removeEventListener('click', digit);
+            button.style.backgroundColor = "gray";
+        });
+    }
+}
+
+let numberButtons = Array.from(document.getElementsByClassName('number')),
+    displayValue = 0;
+display(0);
+numberButtons.forEach(button => button.addEventListener('click', digit));
